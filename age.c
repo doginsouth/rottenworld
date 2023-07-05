b12b6fadbf12532f0d43e572f332b0b3be919d70
southmaddog <13428877173asd@gmail.com>
2023-07-05 13:50:38 -0400

    first bllod

---
+++ age.c
@@ -0,0 +1,11 @@
+//  t h i s  code  i s  written  by Pedram//  importing  header  f i l e s :
+# include <stdio.h>
+int main () {
+int age ;
+float height ;
+printf ( " Enter your age : " ) ;
+scanf ( " % d " , & age ) ;
+printf ( " Enter your height in meters : " ) ;
+scanf ( " % f " , & height );
+printf ( " You are % d years old and %.2 f meters tall .\n " , age , height ) ;
+}
---
+++ world.c
@@ -0,0 +1,8 @@
+# include <stdio.h>
+// the main function
+int main ( void ) {
+/* calling " printf " function
+the " defination " is included in " stdio " library */
+printf("Hello , McMaster!\n ");
+return 0;
+}
